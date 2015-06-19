6539e9d3970a14d1568cbb4b6635b94f9b8f18ef
Larkin Crain <larkincrain@gmail.com>
2015-06-19 18:55:42 -0500

    App.js
    Getting main workflow sketched out.

--- carbonchat/app.js
+++ carbonchat/app.js
@@ -5,17 +5,40 @@
 (function () {
 
     //Our dependences
+    var firebase = require('firebase');
+
     var printer = require('module-print-thermal');
-    var printer = require('module-authenticate');
-    var printer = require('module-message');
+    var authModule = require('module-authenticate');
+    var messageModule = require('module-message');
     
+    //Module level variables
+    var firebaseRef = new Firebase("https://carbonchat.firebaseio.com/");
     
     function init(){
-
+        
         //First things first, lets authenticate
+        authenticate().then();
 
+
+        
     }               //The startup function
-    
+
+    function authenticate(){
+        var authMethods = [authModule.init(firebaseRef), authModule.authenticate()];
+        var result = q();
+
+        authMethods.forEach(function (f) {
+            result = result.then(f);
+        });
+
+        return result.promise;
+    }                   //Authenticate
+    function getMessages(){                      
+
+    }                    //Get any new messages
+    function printMessag(message){
+
+    }            //Print the message that we received
 
     init();     //Get things rolling
 })();
